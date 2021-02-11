88eac5901f62e0188c08532c76a7663749a60dc2
eglushkov <eugeniyglushkov@gmail.com>
2021-02-11 11:25:06 +0300

    New project initiate

---
+++ cdg_swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cdg_swift
+//
+//  Created by evgen on 11.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
